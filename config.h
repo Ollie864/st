38dea43e8ea1c5493c845768eb99b28ee4b2a0d6
ollie <olambert864@protonmail.com>
2022-01-17 19:10:51 +0000

    updated files

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=22antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -132,8 +132,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
+//unsigned int defaultfg = 258;
+//unsigned int defaultbg = 259;
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 232;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
