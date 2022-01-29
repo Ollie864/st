4246be6f5d2da81236decd198ae298b6e1ff4348
ollie <olambert864@protonmail.com>
2022-01-29 08:22:24 +0000

    updated files

--- config.def.h
+++ config.def.h
@@ -132,12 +132,11 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-//unsigned int defaultfg = 258;
-//unsigned int defaultbg = 259;
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 232;
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
+unsigned int background = 0;
 
 /*
  * Default shape of cursor
